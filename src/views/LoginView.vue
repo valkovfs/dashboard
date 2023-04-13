a6f92ec225abe56ac0d52bf831ed41d65a40c16a
Artem valkov <valkov.artyom.fs@gmail.com>
2023-04-13 14:57:16 +0300

    check

---
+++ src/views/LoginView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <button @click="loginWithGithub">Login with GitHub</button>
+  </div>
+</template>
+
+<script>
+import { useAuthStore } from '@/stores/auth';
+
+export default {
+  methods: {
+    async loginWithGithub() {
+      // Access the auth store
+      const authStore = useAuthStore();
+
+      // Call the loginWithGithub action from the auth store
+      await authStore.loginWithGithub();
+    },
+  },
+};
+</script>
