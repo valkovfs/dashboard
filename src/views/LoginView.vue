0f90f9b08d0db55b6b655ca2518b89e77520a513
Artem valkov <valkov.artyom.fs@gmail.com>
2023-04-16 16:19:28 +0300

    fixgit status

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,11 +1,3 @@
-<template>
-    <main class="login-container">
-        <sign-button v-if="!user.isSignedIn" @click="handleRedirect()" :button-text="'Signin with GitHub'"/>
-        <sign-button v-else @click="user.handleSignOut()" :button-text="'Signout'"/>
-
-    </main>
-
-</template>
 
 <script setup lang="ts">
 import { useUserStore } from '@/stores/user';
@@ -17,11 +9,21 @@ import { useRouter } from 'vue-router';
 
 const router = useRouter()
 const handleRedirect = async () => {
-  await user.handleSignInGitHub()
-  await router.push({ path: '/' })
+    await user.handleSignInGitHub()
+    await router.push({ path: '/' })
 
 }
 </script>
+
+
+<template>
+    <main class="login-container">
+        <sign-button v-if="!user.isSignedIn" @click="handleRedirect()" :button-text="'Signin with GitHub'"/>
+        <sign-button v-else @click="user.handleSignOut()" :button-text="'Signout'"/>
+
+    </main>
+
+</template>
 
 <style scoped>
 .login-container {
